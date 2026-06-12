6a8f3981910b4d5b74c2d87a1cf576076ed8ab04
Guilherme Pessoa <guilherme.gpessoa@hotmail.com>
2026-06-11 23:19:34 -0300

    chore: adiciona suporte a variaveis de ambiente e inicia supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  // Inicializa o Supabase usando as variáveis de ambiente
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL'] ?? '',
+    anonKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '',
+  );
+
   runApp(const PokeTcgApp());
 }
 
